45adce12b55a8fdc66cad7d68c673c2eb589ca42
Carlos Benedetti <carlos.lopes@ciag.org.br>
2024-10-08 08:48:39 -0300

    feat: user db

--- app.ts
+++ app.ts
@@ -1,19 +1,41 @@
 import express from "express"
+import sequelize from "./db";
+import { DataTypes, Model, Sequelize } from "sequelize";
+
+
+// DATABASE START
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+//Definindo oque é um usuario 
+class User extends Model { }
+User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+}, { sequelize, modelName: 'user' });
+// salvando a defini de usuario no Banco
+sequelize.sync();
+
 
 const app = express()
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.sendStatus(218)
-})
-
-app.post("/",(pergunta,resposta)=>{
+// CRUD routes for User model
+app.get('/users', async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+});
+app.post("/", (pergunta, resposta) => {
     resposta
         .status(200)
         .send("ola mundo")
 })
 
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("Ouvindo na porta 8080")
 })
